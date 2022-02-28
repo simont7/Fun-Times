249c64e8a49a24c59909478da659c7f9f10b7be6
Simon Johnson <simon.johnson@team7t.com>
2022-02-28 12:50:10 +0000

    Organised imports

--- henrys/src/main/java/com/ford/henrys/service/Discounter.java
+++ henrys/src/main/java/com/ford/henrys/service/Discounter.java
@@ -2,7 +2,6 @@ package com.ford.henrys.service;
 
 import com.ford.henrys.BasketItem;
 import com.ford.henrys.Discount;
-import com.ford.henrys.DiscountRule;
 
 public interface Discounter {
 	Discount discount(BasketItem item);
